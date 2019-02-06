12c010e316a6c6da2f3ddd1d93e4733415158be3
Paul Baecher <pbaecher@gmail.com>
2019-02-06 22:26:30 +0100

    Screens for error and finished

--- frontend/src/healthy/fe/core.cljs
+++ frontend/src/healthy/fe/core.cljs
@@ -6,7 +6,7 @@
             [cljs-http.client :as http]))
 
 (def endpoint "http://localhost:8080")
-(def state (r/atom {:loading true}))
+(defonce state (r/atom {:loading true}))
 
 (defn home []
   [:p "Welcome to agile health check!"])
@@ -27,14 +27,23 @@
                                          :survey-id survey-id
                                          :dimension-id dimension-id
                                          :score score
-                                         :comment comment}}))])))
+                                         :comment comment}}))]
+        (if (= (:status response) 201)
+          (swap! state update-in [:dimension] inc)
+          (swap! state assoc :error? true)))))
 
-(defn survey [s]
+(defn all-done [s]
+  [:div.center
+   [:h1 "All done!"]
+   [:p.emoji "🎉"]
+   [:p "Thank you for your feedback."]])
+
+(defn survey-step [s]
   [:div
    [:h1 (str
           (get-in s [:survey :template :title])
           " – "
-          (:name ((get-in s [:survey :template :dimensions]) 0)))]
+          (:name ((get-in s [:survey :template :dimensions]) (:dimension s))))]
    [:div#panel
     (concat (for [index (range 5)]
               ^{:key index} [:p
@@ -62,37 +71,53 @@
                       :on-change #(swap! state assoc :comment (-> % .-target .-value))
                       :value (:comment s)}]])])))]])
 
+(defn survey [s]
+  (if (< (:dimension s) (count (-> s :survey :template :dimensions)))
+    (survey-step s)
+    (all-done s)))
+
 (defn loading-display []
   [:p "Loading..."])
 
+(defn error []
+  [:div.center
+   [:h1 "Oh snap!"]
+   [:p.emoji "😭"]
+   [:p "Something went very wrong."]])
+
 (defn root []
   (let [s @state]
-    (if (:loading s)
-      (loading-display)
-      (case (:screen s)
-        :take-survey (survey s)
-        (home)))))
-
-(defn main []
-  (r/render [root] (.getElementById js/document "app")))
-
-(main)
+    (cond
+      (:error? s) (error)
+      (:loading s) (loading-display)
+      :else (case (:screen s)
+              :take-survey (survey s)
+              (home)))))
 
 (defroute "/survey/:id" {:as params}
   (go
     (let [response (<! (http/get
                          (str endpoint "/api/query/survey/" (:id params))
                          {:with-credentials? false}))]
-      (swap! state merge
-             {:loading false
-              :screen :take-survey
-              :survey-id (:id params)
-              :survey (:body response)}))))
+      (if (= (:status response) 200)
+        (swap! state merge
+               {:loading false
+                :screen :take-survey
+                :dimension 0
+                :survey-id (:id params)
+                :survey (:body response)})
+        (swap! state assoc :error? true)))))
 
 (defroute "*" []
   (swap! state assoc :loading false :screen :home))
 
-(secretary/dispatch! (subs js/location.hash 1))
+(defn main []
+  (r/render [root] (.getElementById js/document "app")))
 
 (set! (.-onhashchange js/window)
       (fn [_] (secretary/dispatch! (subs js/location.hash 1))))
+
+(when (:loading @state)
+ (secretary/dispatch! (subs js/location.hash 1)))
+
+(main)
